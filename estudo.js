dddf4037f518140fe7e721c4d9001b6d47f646ca
lyndossantos <108936981+lyndossantos@users.noreply.github.com>
2022-10-20 11:11:23 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -39,4 +39,25 @@ function total(){
         v = r;
     }
     document.write("Resultado: " + r);
+
+}
+
+function Notas(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+
+    let r = Number(n1)+Number(n2)+Number(n3)+Number(n4)
+    document.getElementById("resul").innerHTML = "soma:" + r;
+}
+
+function Media(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+
+    let r = Number(n1)+Number(n2)+Number(n3)+Number(n4)
+    document.getElementById("resul").innerHTML = "soma:" + r;
 }
